5ae625833732e4d25fd105b0097e359ac30329fe
abetker <adam.betker@gmail.com>
2016-09-14 14:21:50 -0500

    Added Letter templates that print out.

--- start.py
+++ start.py
@@ -31,6 +31,9 @@ raptors = rank_sort[2::3]
 ###########################################
 # Letter Printing Section
 ##########################################
+dragons_date = "March 17, 1pm"
+sharks_date = "March 17, 3pm"
+raptors_date = "March 18, 1pm"
 
 letter_to_parent = """
 
@@ -38,55 +41,30 @@ Dear {},
 Your child, {} has been assigned to Team {}.  The first practice is scheduled for {}.
 We look forward to seeing your child there!
 
+Sincerely,
+Coach Betker
 
-
-
-                           ,;;;:.
-                          ;;''''`:
-                          ;(  O O|              ,;;,
-                           |   _\|              \  |
-                            \__-/              ,' /
-                            |   |             /  /
-                      _,--''`---'''-------.,-'  /
-                    ,'  /          `.     |  _,'
-                  ,'    |====== WM =|     |-'
-                 \      ,======|  |=|''---'
-                / `.  ,' \      \/ /
-              ,'. ,'`'   | --._    |
-            ,'  ,'       |         |
-        __,' _,'         \    -._  |
-       `- ,-'            |---------)
-      ';;'               ;:::::::::|
-                        ;:::::::::::\
-                       /::::::;:::::|
-                      /_:::::/\:::::_\
-                      / `-:_/  \,-' |
-                     /    /     \   |
-                     |   |      | _,)
-                     \_,-\      |   |
-                      \   |     |   |
-                       |__|      \,-|
-                       /##|      |  |
-                      \##/       |  |
-                     ,-'''-.     |,-|
-                    // \_/ \\    `.##\
-                    |\_/ \_/|      `--`
-                    \/ \_/ \/
-                     `-...-'
  ___  ___   ___ ___ ___ _ __
 / __|/ _ \ / __/ __/ _ \ '__|
 \__ \ (_) | (_| (_|  __/ |
 |___/\___/ \___\___\___|_|
 
-
 """
 
 
+for player in dragons:
+    print(letter_to_parent.format(player[3],player[0],"Dragons", dragons_date))
+
+for player in sharks:
+    print(letter_to_parent.format(player[3],player[0],"Sharks", sharks_date))
+
+for player in raptors:
+    print(letter_to_parent.format(player[3],player[0],"Raptors", raptors_date))
 
 
-print(len(dragons))
-print(len(sharks))
-print(len(raptors))
-print(letter_to_parent)
+# print(dragons)
+# print(len(sharks))
+# print(len(raptors))
+# print(letter_to_parent)
 
 
